e8d7eb020c24b2bcdbf53f05233bb2ddd3c01a99
Kirill <wakened90@gmail.com>
2022-12-23 15:26:01 +0300

    2nd digit working

--- 2nd_digit_of_3digitnumber/Program.cs
+++ 2nd_digit_of_3digitnumber/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Second_digit_of_3 (int number)
+{
+    int digit;
+
+    if (number > 99 & number < 1000)
+    {
+        digit = number % 100;
+        digit = digit / 10;
+        Console.Write($"Вторая цифра числа = {digit}");
+    }
+    else
+    {
+        Console.Write("Вы ввели не трёхзначное число");
+    }
+}
+
+int num;
+
+Console.Write("Введите трёхзначное число: ");
+num = int.Parse(Console.ReadLine()!);
+
+Second_digit_of_3 (num);
