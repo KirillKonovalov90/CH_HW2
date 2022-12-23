5502b471d84d92f60c9db0cc524a8d3c91805d41
Kirill <wakened90@gmail.com>
2022-12-23 17:34:08 +0300

    print_3rd_digit working

--- print_3rd_digit/Program.cs
+++ print_3rd_digit/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Print_3rd_digit(int number)
+{
+    int digit;
+
+    if (number < 99)
+    {
+        digit = -1;
+    }       
+    else
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        digit = number % 10;           
+    }
+    return digit;
+}
+
+int result, num;
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine()!);
+
+result = Print_3rd_digit(num);
+
+if (result != -1)
+{
+    Console.Write(result);
+}
+else
+{
+    Console.Write("У числа отсутствует 3я цифра");
+}
+
