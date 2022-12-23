801b81edc14afe9920e4c4ebbaaf7242e9075531
Kirill <wakened90@gmail.com>
2022-12-23 18:04:31 +0300

    day_of_the_week working

--- day_of_the_week/Program.cs
+++ day_of_the_week/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Number_to_day_of_the_week(int number)
+{
+    if (number > 5 && number <= 7)
+    {
+        Console.WriteLine("ДА");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+}
+
+int num;
+
+Console.Write("Введите номер дня недели, проверим выходной ли он: ");
+num = int.Parse(Console.ReadLine()!);
+
+if (num < 1 || num > 7 )
+{
+    Console.WriteLine($"Число {num} некорректно для дня недели, введите от 1 до 7");    
+}
+else
+{
+    Number_to_day_of_the_week(num);
+}
