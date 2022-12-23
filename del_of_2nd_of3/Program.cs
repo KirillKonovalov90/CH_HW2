ab898836ba6056dff349823cd8ec70ba77f405b7
Kirill <wakened90@gmail.com>
2022-12-23 17:04:29 +0300

    del_the_2nd_of_3 working

--- del_of_2nd_of3/Program.cs
+++ del_of_2nd_of3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void del_the_2nd_of_3randommed()
+{
+    int number, result, first, last;
+
+    number = new Random().Next(100, 1000);     
+    first = number / 100 * 10;
+    last = number % 10;
+    result = first + last;
+    Console.Write($"Из числа {number} удаляем 2ю цифру, получаем: {result}");
+}
+
+del_the_2nd_of_3randommed();
